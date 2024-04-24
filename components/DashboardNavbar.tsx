092b636d70211b1e0fea0acb408348497198f399
Vladimir Pasev <vladigpasev@gmail.com>
2024-04-24 22:12:33 +0300

    Updated something

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -16,11 +16,20 @@ const DashboardNavbar = () => {
     const DotIcon = () => {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-    <path d="M73.44 205.63l212.31 212.31 389.86-389.86-212.31-212.31-389.86 389.86zm847.96-290.63L830.9 91.58c-119.46 119.46-164.2-164.2-164.2-164.2l-90.1-90.1c-60.73 0-60.73 60.73 0 60.73l90.1 90.1c0 0 283.66-44.74 164.2 164.2l90.1 90.1c60.73 0 60.73-60.73 0-60.73zm-212.31 997.93l-309.56-309.54L464.4 512l309.55 309.55-212.31 212.31z" fill="currentColor"/>
-</svg>
+                <path d="M73.44 205.63l212.31 212.31 389.86-389.86-212.31-212.31-389.86 389.86zm847.96-290.63L830.9 91.58c-119.46 119.46-164.2-164.2-164.2-164.2l-90.1-90.1c-60.73 0-60.73 60.73 0 60.73l90.1 90.1c0 0 283.66-44.74 164.2 164.2l90.1 90.1c60.73 0 60.73-60.73 0-60.73zm-212.31 997.93l-309.56-309.54L464.4 512l309.55 309.55-212.31 212.31z" fill="currentColor" />
+            </svg>
 
         )
     }
+
+    const OrganizeIcon = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M20,2H4C2.9,2,2,2.9,2,4v16c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2V4C22,2.9,21.1,2,20,2z M12,20H4v-4h8V20z M12,14H4v-4h8V14z M20,20h-6v-4h6V20z M20,14h-6v-4h6V14z"/>
+            </svg>
+        )
+    }
+    
 
     return (
         <nav className={`w-full navbar-transition z-50 bg-white shadow-md transition-all duration-300 ease-in-out py-3`}>
@@ -33,6 +42,11 @@ const DashboardNavbar = () => {
                                 label="Моите билети"
                                 url="/my-tickets"
                                 labelIcon={<DotIcon />}
+                            />
+                            <UserButton.UserProfileLink
+                                label="Организирай"
+                                url="https://organize.eventify.bg/"
+                                labelIcon={<OrganizeIcon />}
                             />
                         </UserButton>
                     </div>
